bbc5401e84283311e98ce9c821be3c912929eb4d
ilya-skoropad <ilya.skoropad0@gmail.com>
2022-03-13 20:46:50 +0300

    some test stuff

---
+++ hw2_signer/go.mod
@@ -0,0 +1,3 @@
+module lol/hw2_signer
+
+go 1.17
--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,45 @@
 package main
 
-// сюда писать код
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"sync"
+)
+
+func main() {
+	prepareWorkers()
+}
+
+func prepareWorkers() {
+	wg := &sync.WaitGroup{}
+	dataChanel := make(chan int)
+
+	wg.Add(2)
+	go generateData(dataChanel, wg)
+	go ProcessData(dataChanel, wg)
+
+	wg.Wait()
+}
+
+func generateData(dataChanel chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for i := 0; i <= 10; i++ {
+		dataChanel <- i
+	}
+	close(dataChanel)
+}
+
+func ProcessData(dataChanel <-chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for data := range dataChanel {
+		fmt.Printf("%x\n", GetMD5Hash(fmt.Sprint(data)))
+	}
+}
+
+func GetMD5Hash(text string) string {
+	hash := md5.Sum([]byte(text))
+	return hex.EncodeToString(hash[:])
+}
